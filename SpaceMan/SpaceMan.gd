5bb622c532940805266268abcaed10fc9fe96045
graham <graham.cr.baker@gmail.com>
2024-01-27 13:31:33 -0800

    changing cat background

--- SpaceMan/SpaceMan.gd
+++ SpaceMan/SpaceMan.gd
@@ -6,10 +6,8 @@ const SPRITE_0002 = preload("res://SpaceMan/space-sprites/Sprite-0002.png")
 @onready var punchline = $punchline
 
 func _ready():
-	Input.set_custom_mouse_cursor(SPRITE_0002, Input.CURSOR_ARROW, Vector2())
-
-
-
+	#Input.set_custom_mouse_cursor(SPRITE_0002, Input.CURSOR_ARROW, Vector2())
+	pass
 
 
 func _on_control_gui_input(event):
@@ -18,9 +16,24 @@ func _on_control_gui_input(event):
 		timer.start()
 		punchline.show()
 		ship.show()
-		
+
 		ship.launch_ship()
 
 
 func _on_timer_timeout():
 	wrap_up_joke()
+
+
+func _on_control_2_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("explode")
+
+
+func _on_control_3_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("explode")
+
+
+func _on_control_4_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("explode")
