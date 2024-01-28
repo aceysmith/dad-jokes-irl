3cd0141c668d344b8447a21bbddb354799b78f5f
Adam Christopher Smith <aceysmith@gmail.com>
2024-01-28 12:45:09 -0800

    Tweaked bike joke difficulty, added sleeping noises

--- Bike/bike.gd
+++ Bike/bike.gd
@@ -1,10 +1,35 @@
 extends BaseJoke
 @onready var bike_sprite = $BikeSprite
+@onready var sleeping_noises = $SleepingNoises
+@onready var thought_cloud = $SleepingNoises/ThoughtCloud
+
+var difficulty: float = 1.001
+var begun:bool = false
+var bike_fell: bool = false
+
+func _ready():
+	instructions_hidden.connect(func():
+		begun = true
+	)
 
 func _process(delta):
+	if !begun or bike_fell:
+		return
+
+	if abs(bike_sprite.rotation_degrees) < .001:
+		bike_sprite.rotation_degrees += 1
+
+	difficulty += .00001
 	if bike_sprite.rotation_degrees > -90 and bike_sprite.rotation_degrees < 90:
-		bike_sprite.rotation = bike_sprite.rotation * 1.01
+		bike_sprite.rotation_degrees = bike_sprite.rotation_degrees * difficulty
 	else:
+		bike_fell = true
+		sleeping_noises.show()
+		if bike_sprite.rotation_degrees > 0:
+			sleeping_noises.position = Vector2(650, 500)
+		else:
+			thought_cloud.flip_h = true
+			sleeping_noises.position = Vector2(100, 500)
 		wrap_up_joke()
 
 func _input(event: InputEvent):
