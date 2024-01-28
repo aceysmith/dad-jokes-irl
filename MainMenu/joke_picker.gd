ebedea43037795ee2a8c3ad56e97bf521d7e051a
Adam Christopher Smith <aceysmith@gmail.com>
2024-01-28 14:52:28 -0800

    Added and cleaned up main screen

--- MainMenu/joke_picker.gd
+++ MainMenu/joke_picker.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @onready var jokes_box = %JokesBox
-@export var auto_play = false
-@onready var you_win_screen = %YouWinScreen
+var auto_play = false
+
+@onready var main_screen = %MainScreen
+@onready var free_play_button = %FreePlayButton
+
 @onready var background_music = %BackgroundMusic
 
-
+var all_jokes: Array[PackedScene] = []
 var remaining_jokes: Array[PackedScene] = []
 
 # Called when the node enters the scene tree for the first time.
@@ -18,37 +21,44 @@ func _ready():
 			push_error("Scene not fully configured, removing from list")	
 			joke_button.queue_free()
 		else:
-			remaining_jokes.append(joke_button.joke_packed_scene)
+			all_jokes.append(joke_button.joke_packed_scene)
 			joke_button.joke_selected.connect(show_joke)
 
-	if auto_play:
-		background_music.play()
-		remaining_jokes.shuffle()
-		show_next_joke()
+	background_music.play()
+
+func _on_play_button_pressed():
+	remaining_jokes = all_jokes.duplicate()
+	remaining_jokes.shuffle()
+
+	auto_play = true
+	show_next_joke()
+	main_screen.hide()
+
+func _on_free_play_button_pressed():
+	auto_play = false
+	jokes_box.show()
+	main_screen.hide()
 
 func show_next_joke():
 	if len(remaining_jokes) > 0:
 		show_joke(remaining_jokes.pop_front())
 	else:
-		you_win_screen.visible = true
+		free_play_button.show()
+		main_screen.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func show_joke(joke_scene: PackedScene):
 	var joke = joke_scene.instantiate() as BaseJoke
 	joke.joke_ended.connect(dismiss_joke)
 	add_child(joke)
-	jokes_box.visible = false
+	jokes_box.hide()
 
 func dismiss_joke(node: Node):
-	jokes_box.visible = true
 	node.queue_free()
 	if auto_play:
 		show_next_joke()
-
-func enable_free_play():
-	auto_play = false
-	jokes_box.visible = true
-	you_win_screen.visible = false
+	else:
+		main_screen.show()
 
 func replay_music():
 	background_music.play()
